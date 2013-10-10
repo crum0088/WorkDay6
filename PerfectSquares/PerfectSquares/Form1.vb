08165e27aa0b4ef252d96614ed4bc17a5abd3133
Charles Crump <crum0088@umn.edu>
2013-10-09 22:31:58 -0500

    I worked on work day 6

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,8 +1,15 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
-
         lstResult.Items.Clear()
+        Dim i As Integer
+
+        Do While (i <= 10)
+            lstResult.Items.Add("The Perfect Square: " & i ^ 2)
+            i = i + 1
+        Loop
+
+
         lstResult.Items.Add("Not implemented yet")
 
     End Sub
